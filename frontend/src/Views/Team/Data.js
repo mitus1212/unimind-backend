286319196d7748cfe770474b6db4ab2d612e2de5
Krzysztov <krzysiek.radej.ken@gmail.com>
2022-02-28 23:44:30 +0100

    w sumie to cakiem spoko

---
+++ frontend/src/Views/Team/Data.js
@@ -0,0 +1,52 @@
+import image from "../../assets/images/card_photos/saczyyy.jpg";
+
+export const teamData = [
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+  {
+    name: "Piotr Saczuk",
+    nick: "Saczyyy",
+    role: "Founder",
+    image: { image },
+  },
+];
