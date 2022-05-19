b4180cb8b513c000dc439e34d7f1095d2ee69f7b
mitus1212 <36314619+mitus1212@users.noreply.github.com>
2022-05-19 23:43:33 +0200

    update

--- frontend/src/Components/Elements/Title/Title.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Title = (props) => <h1 className="views__title">{props.children}</h1>;
-
-export default Title;
--- frontend/src/Components/Onepage/Sections/Guilds/Guilds.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Guilds = () => <div>Guilds site</div>;
-
-export default Guilds;
